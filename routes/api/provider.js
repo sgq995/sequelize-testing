297a43038b72d07b03173545a2a593142a7fbedf
Sebastian G. Q <sgq995@gmail.com>
2021-06-29 21:40:46 -0500

    feat(provider): Added read and create to provider's route

--- routes/api/provider.js
+++ routes/api/provider.js
@@ -1,12 +1,96 @@
 const express = require('express');
+const { Provider } = require('../../models');
+
 const router = express.Router();
 
 router.route('/')
-  .get('/', function (req, res, next) {
+  .get(async function (req, res, next) {
+    try {
+      const providers = await Provider.findAll({
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        },
+        include: [{
+          association: 'parts',
+          attributes: {
+            exclude: ['ProviderId', 'createdAt', 'updatedAt']
+          }
+        }]
+      });
 
+      res
+        .status(200)
+        .json({
+          status: 200,
+          data: providers
+        });
+    } catch (err) {
+      next(err);
+    }
   })
-  .post('/', function (req, res, next) {
+  .post(async function (req, res, next) {
+    const { body } = req;
 
+    try {
+      const provider = await Provider.upser(body);
+
+      res
+        .status(201)
+        .json({
+          status: 201,
+          data: {
+            id: provider.id
+          }
+        });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+router.route('/:providerId')
+  .get(async function (req, res, next) {
+    const { providerId } = req.params;
+
+    try {
+      const provider = await Provider.findByPk(providerId, {
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        },
+        include: [{
+          association: 'parts',
+          attributes: {
+            exclude: ['ProviderId', 'createdAt', 'updatedAt']
+          }
+        }]
+      });
+
+      res
+        .status(201)
+        .json({
+          status: 201,
+          data: provider
+        });
+    } catch (err) {
+      next(err);
+    }
+  })
+  .put(async function (req, res, next) {
+    const { providerId } = req.params;
+
+    try {
+
+    } catch (err) {
+
+    }
+  })
+  .delete(async function (req, res, next) {
+    const { providerId } = req.params;
+
+    try {
+
+    } catch (err) {
+
+    }
   });
 
 module.exports = router;
